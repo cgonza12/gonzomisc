abe15cc19d5a446f7532ad2d0788831bba7e69ae
Christian Gonzalez <christiangonzalez357@gmail.com>
2017-10-18 12:52:08 -0700

    Add files via upload

--- kanoAnalysis.R
+++ kanoAnalysis.R
@@ -1,11 +1,11 @@
-### Convert Kano response strings to numeric responses, user must provide single character vector and question type (either functional or dysfunctional)
 kano.convert2numeric <- function(x,questionType = NULL,
                          expectString = 'I expect it',
                          likeString = 'I like it',
                          neutralString = "I'm neutral",
                          dealwithString = "I'll deal with it",
                          dislikeString = "I dislike it"){
- 
+  ### Convert Kano response strings to numeric responses, user must provide single character vector and question type (either functional or dysfunctional)
+  
   x[x==expectString] = 'expect'
   x[x==likeString] = 'like'
   x[x==neutralString] = 'neutral'
@@ -31,14 +31,14 @@ return(as.numeric(x))
 
 
 
-### Compute classification for feature based on functional and dysfucntional,  
 kano.getClass <- function(x,order = 1,
                                  expectString = 'I expect it',
                                  likeString = 'I like it',
                                  neutralString = "I'm neutral",
                                  dealwithString = "I'll deal with it",
                                  dislikeString = "I dislike it"){
-
+### Compute classification for feature based on functional and dysfucntional questions, order 1 assumes first col is functional, order 2 assumes dysfuctional is first
+  
 #Function accepts two column data.frame
 
 if(ncol(x)!=2){print('incorrect number of cols')}
@@ -51,12 +51,6 @@ kanoKey = data.frame(
   dislike = c("R","R","R","R","Q"))
 row.names(kanoKey) <- c('like','expect','neutral','deal','dislike')
 
-expectString = 'I expect it'
-likeString = 'I like it'
-neutralString = "I'm neutral"
-dealwithString = "I'll deal with it"
-dislikeString = "I dislike it"
-
 x[x==expectString] = 'expect'
 x[x==likeString] = 'like'
 x[x==neutralString] = 'neutral'
@@ -65,14 +59,19 @@ x[x==dislikeString] = 'dislike'
 
 if(order == 1){
 kanoClass <- sapply(1:nrow(x),function(i){
-  as.character(kanoKey[as.character(x[i,1]),
-          as.character(x[i,2])])})}
+  as.character(kanoKey[as.character(x[i,2]),
+          as.character(x[i,1])])})}
 
 if(order == 2){
   kanoClass <- sapply(1:nrow(x),function(i){
-    as.character(kanoKey[as.character(x[i,2]),
-                         as.character(x[i,1])])})}
+    as.character(kanoKey[as.character(x[i,1]),
+                         as.character(x[i,2])])})}
 
 return(kanoClass)
 }
 
+kano.getClassNumeric <- function(func,dys){ifelse((func <= 2 & dys <= -1) | (func <= -1 & dys <= 2), "R",
+                                                  ifelse((func <= 2 & func >= -1 & dys <= 2& dys >= -1), "I",
+                                                         ifelse((func >= 2 & dys >= -1 & dys <= 2), "A",
+                                                                ifelse((func <= 2 & dys <= 4 & dys >= 2), "M",
+                                                                       ifelse((func >= 2 & dys >= 2), "P","Q")))))}
